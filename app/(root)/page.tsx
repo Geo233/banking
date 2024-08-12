259e773e7295d14f239f37ae1a806b55852bdec7
Negrau George <george.negrau@e-uvt.ro>
2024-08-12 16:48:37 +0300

    seccond commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
+import RightSidebar from "@/components/RightSidebar"
+import HeaderBox from "@/components/ui/HeaderBox"
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox"
 
-import React from 'react'
+
 
 const Home = () => {
+  const loggedIn = { firstName: 'George' }
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
+    </section>
   )
 }
 
