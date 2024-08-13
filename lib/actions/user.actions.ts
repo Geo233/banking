1611c5f90f3295c3f0664646f433079525ff44a9
Negrau George <george.negrau@e-uvt.ro>
2024-08-13 18:18:21 +0300

    sign-in sign-up implementation

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+'use server';
+
+export const signIn = async () => {
+    try{
+        //Mutation / Database / Make fetch
+    } catch (error){
+        console.error('Error', error);
+    }
+
+}
+
+export const signUp = async () => {
+    try{
+        //Mutation / Database / Make fetch
+    } catch (error){
+        console.error('Error', error);
+    }
+
+}
